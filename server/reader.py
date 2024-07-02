451b864552de87d0f2965b384e87412c011bd34d
Jason Chen <jchen.012004@gmail.com>
2024-07-02 14:28:23 -0500

    connect front end to server

--- server/reader.py
+++ server/reader.py
@@ -1,10 +1,10 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import csv
 import gspread
 import time
 
-sources = ['wells_fargo', 'discover', 'capital_one']
-months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-transactions =[]
+transactions = []
 
 def wellsFargoTrans(file):
     try:
@@ -61,16 +61,66 @@ sa = gspread.service_account()
 sh = sa.open("Expenses 2")
 
 
-for month in months:
+def write_data(f):
+    print("write")
     transactions = []
-    file = f"wells_fargo_{month}.csv"
-    rows = wellsFargoTrans(file)
-    file = f"discover_{month}.csv"
-    discoverTransactions(file)
+    cur_file = f['file']
+    if f['bank'] == "Wells Fargo":
+        rows = wellsFargoTrans(cur_file)
+    elif f['bank'] == "Discover":
+        rows = discoverTransactions(cur_file)
+    elif f['bank'] == "Capital One":
+        rows = capitalOneTransactions(cur_file)
 
-    wks = sh.worksheet(f"{month}")
+    print("after transactions")
+
+    wks = sh.worksheet(f"{f['month']}")
+    print("after wks")
     for row in rows:
+        print("in loop")
         wks.insert_row([row[0], row[2], row[3], row[1]], 7)
         time.sleep(2)
+    print("after writes")
     wks.update_acell('B2', '=SUMIF(D7:D,">0")')
     wks.update_acell('B3', '=SUMIF(D7:D,"<0")')
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/files": {"origins": "http://localhost:3000"}})
+
+
+@app.route('/members')
+def members():
+    return {'members': ["Member 1", "Member 2", "Member 3"]}
+
+@app.route('/files', methods=['GET','POST'])
+def files():
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
+        file = request.files['file']
+        file_name = request.form.get('fileName')
+        month = request.form.get('month')
+        bank = request.form.get('bank')
+
+        print(f"Received file: {file_name}, month: {month}, bank: {bank}")
+
+
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        print("before write")
+
+        print("written")
+
+
+        return jsonify({'message': 'File successfully uploaded', 'fileName': file_name, 'month': month, 'bank': bank}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
